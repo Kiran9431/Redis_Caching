8f2d5b6e74b620283941f2f5bd668b01e499e4a6
Kiran9431 <138762229+Kiran9431@users.noreply.github.com>
2023-10-11 23:59:57 +0400

    Initial commit

---
+++ Redis_Caching/src/main/java/com/example/Redis_Caching/entity/Employee.java
@@ -0,0 +1,20 @@
+package com.example.Redis_Caching.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+import java.io.Serializable;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@RedisHash("Employee")
+public class Employee implements Serializable {
+    @Id
+    private int id;
+    private String name;
+    private String dept;
+    private int salary;
+}
